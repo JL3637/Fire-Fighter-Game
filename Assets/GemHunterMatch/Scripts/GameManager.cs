c423633cce17e7a47d139e3daa3d40224cc66625
JL3637 <jason36375667@gmail.com>
2024-10-04 02:06:26 +0800

    good

--- Assets/GemHunterMatch/Scripts/GameManager.cs
+++ Assets/GemHunterMatch/Scripts/GameManager.cs
@@ -207,7 +207,7 @@ namespace Match3
 
             halfSize += LevelData.Instance.BorderMargin;
         
-            Camera.main.orthographicSize = halfSize;
+            Camera.main.orthographicSize = halfSize + 2;
         }
 
         /// <summary>
